204d579a2f3d597fedd02820621ab2e79ae12afe
tim-t_miyata <t.miyata@logiclogic.jp>
2020-01-31 20:35:37 +0900

    ブロック内でhtmlメソッドをパースする

--- sumo_get_html.rb
+++ sumo_get_html.rb
@@ -6,14 +6,9 @@ require 'open-uri'
 search_url = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&pc=30&smk=&po1=12&po2=99&shkr1=03&shkr2=03&shkr3=03&shkr4=03&rn=0005&ek=000500480&ra=013&cb=0.0&ct=9999999&md=02&et=9999999&mb=0&mt=9999999&cn=9999999&fw2='
 base_url = 'https://suumo.jp/'
 
-charset = nil
-
-html = open(search_url) do |f|
-  charset = f.charset
-  f.read
+doc = open(search_url) do |f|
+  Nokogiri::HTML.parse(f.read, nil, f.charset)
 end
-
-doc = Nokogiri::HTML.parse(html, nil, charset)
 
 doc.xpath('//ul[@class="l-cassetteitem"]').each.with_index do |node, i|
   p '住宅名:' + node.xpath('//div[@class="cassetteitem_content-title"]')[i].inner_text
