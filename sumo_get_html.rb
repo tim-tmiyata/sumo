cb40fd9645c70eaab6ca9842c2c28443b068e938
tim-t_miyata <t.miyata@logiclogic.jp>
2020-01-31 20:46:22 +0900

    Structを使ってデータを構造化する

--- sumo_get_html.rb
+++ sumo_get_html.rb
@@ -6,13 +6,19 @@ require 'open-uri'
 SEARCH_URL = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&pc=30&smk=&po1=12&po2=99&shkr1=03&shkr2=03&shkr3=03&shkr4=03&rn=0005&ek=000500480&ra=013&cb=0.0&ct=9999999&md=02&et=9999999&mb=0&mt=9999999&cn=9999999&fw2='
 BASE_URL = 'https://suumo.jp/'
 
+Property = Struct.new('Building', :title, :address, :rent, :url)
+
 doc = open(SEARCH_URL) do |f|
   Nokogiri::HTML.parse(f.read, nil, f.charset)
 end
 
 doc.xpath('//ul[@class="l-cassetteitem"]').each.with_index do |node, i|
-  p '住宅名:' + node.xpath('//div[@class="cassetteitem_content-title"]')[i].inner_text
-  p '住所:' + node.xpath('//li[@class="cassetteitem_detail-col1"]')[i].inner_text
-  p '家賃:' + node.xpath('//span[@class="cassetteitem_other-emphasis ui-text--bold"]')[i].inner_text
-  p '詳細ページ:' + File.join(BASE_URL, node.xpath('//td[@class="ui-text--midium ui-text--bold"]//a')[i][:href])
+  property = Property.new(node.xpath('//div[@class="cassetteitem_content-title"]')[i].inner_text,
+                          node.xpath('//li[@class="cassetteitem_detail-col1"]')[i].inner_text,
+                          node.xpath('//span[@class="cassetteitem_other-emphasis ui-text--bold"]')[i].inner_text,
+                          File.join(BASE_URL, node.xpath('//td[@class="ui-text--midium ui-text--bold"]//a')[i][:href]))
+  p "住宅名: #{property.title}"
+  p "住所: #{property.address}"
+  p "家賃: #{property.rent}"
+  p "詳細ページ: #{property.url}"
 end
